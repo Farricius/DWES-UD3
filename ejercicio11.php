85d527c4ac903453115a9d4e44202e67daec45df
Farricius <jose.hernandez.chacon.al@iespoligonosur.org>
2021-10-05 14:27:30 +0200

    Casa

---
+++ ejercicio11.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <table border="5">;
+
+        <?php
+
+        $cont = 0;
+
+        for ($i = 0; $i < 10; $i++) {
+
+            if ($i % 2 == 0) {
+                echo "<tr style = 'background-color:white'>";
+            } else {
+                echo "<tr style = 'background-color:olive'>";
+            }
+
+            for ($j = 0; $j < 10; $j++) {
+                $cont++;
+                echo "<td>" . $cont . "</td>";
+            }
+
+            echo "</tr>";
+        }
+
+        ?>
+    </table>;
+
+</body>
+
+</html>
--- ejercicio9.php
+++ ejercicio9.php
@@ -10,6 +10,9 @@
 
 <body>
     <?php
+
+    $date = new DateTime("2021-10-04");
+
     $fechaNacimiento = 1955;
 
     $fechaDiscoteca = date('Y') - $fechaNacimiento;
@@ -31,16 +34,23 @@
     echo $interval->format('Esta persona tiene: %Y Años, %m meses y %d días');
 
     echo "<br>";
+    echo "APARTADO 2";
+    echo "<br>";
 
     //echo strtotime($fechaHoy);
 
-    
-    if ($interval > 65) {
-        echo "NO puede entrar, es anciano";
-    } elseif ($interval > 18) {
-        echo "Es  mayor para entrar";
+    $fecha_nacimiento = "04-10-2003";
+    $dia_actual = date ("Y-m-d");
+    $edad_diff = date_diff(date_create($fecha_nacimiento) , date_create($dia_actual));
+    //var_dump($edad_diff);
+    echo $edad_diff->y;
+
+    if ($edad_diff > 65) {
+        echo " NO puede entrar, es anciano";
+    } elseif ($edad_diff > 18) {
+        echo " Es  mayor para entrar";
     } else {
-        echo "Es menor edad, no puede";
+        echo " Es menor edad, no puede";
     }
 
     ?>
